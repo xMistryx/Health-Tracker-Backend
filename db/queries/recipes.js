5385a880ff5ac1058a611af402881c881e6a6c86
EAPV2 <eapv28@hotmail.com>
2025-09-29 11:16:29 -0400

    fixed recipes

--- db/queries/recipes.js
+++ db/queries/recipes.js
@@ -30,9 +30,15 @@ export async function addRecipe(userId, { title, image_url, description, ingredi
 export async function updateRecipe(id, userId, { title, image_url, description, ingredients, instructions, created_by }) {
   const sql = `
     UPDATE recipes
-    SET title = $3, image_url = $4, description = $5, ingredients = $6, instructions = $7, created_by = $8
-    WHERE id = $1 AND user_id = $2
-    RETURNING *
+SET title = $3,
+    image_url = $4,
+    description = $5,
+    ingredients = $6,
+    instructions = $7,
+    created_by = $8,
+    updated_at = NOW()
+WHERE id = $1 AND user_id = $2
+RETURNING *
   `;
   const { rows: [recipe] } = await db.query(sql, [id, userId, title, image_url, description, JSON.stringify(ingredients), instructions, created_by]);
   return recipe;
