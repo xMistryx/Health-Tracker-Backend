4aabba59b42b75428488ab708fb1a877ed6e5c90
Jasmine Gutierrez <Jasmineg01@comcast.net>
2025-10-05 02:53:16 -0500

    route changes

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  // Only apply getUserFromToken to protected routes, not /users/register or /users/login
   if (req.path === '/users/register' || req.path === '/users/login') {
     return next();
   }
