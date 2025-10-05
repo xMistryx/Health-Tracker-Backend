3af895bf2dee48722df534956e6c9aa401fdf73b
Jasmine Gutierrez <Jasmineg01@comcast.net>
2025-10-05 03:00:48 -0500

    route changes

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  if (req.path === '/users/register' || req.path === '/users/login') {
-    return next();
-  }
-  getUserFromToken(req, res, next);
-});
-
 app.use("/users", usersRouter);
 app.use("/health_info", health_infoRouter);
 app.use("/water_logs", water_logsRouter);
